d7ecdbb70365342643c37dad240cd120ee72811d
ana_sorina.ciurea <ana_sorina.ciurea@stud.acs.upb.ro>
2025-01-11 13:49:28 +0200

    command to go
    Signed-off-by: ana_sorina.ciurea <ana_sorina.ciurea@stud.acs.upb.ro>

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -2,6 +2,54 @@ package org.example;
 
 public class Person {
     private String surname;
-    
+    private String name;
+    private String role;
+    private int age;
+    private String email;
 
+    public Person(String surname, String name, String role) {
+        this.surname = surname;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Student extends Person {
+    private String school;
+    private int studyYear;
+
+    public Student(String surname, String name, String role) {
+        super(surname, name, role);
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getStudyYear() {
+        return studyYear;
+    }
+
+    public void setStudyYear(int studyYear) {
+        this.studyYear = studyYear;
+    }
+}
